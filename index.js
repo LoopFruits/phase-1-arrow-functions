09e3bfaaaaacc01cac1b689aed145c5afa2b2908
Kenneth Jordan <kensworldc@gmail.com>
2022-01-18 13:41:11 -0500

    completed assignment

---
+++ index.js
@@ -0,0 +1,25 @@
+const divide = function(){
+    return 2000 / 100
+}
+
+const square = x => 4
+// the question is asking for a parameter that multiplies itself to equal for. but i just wrote an arrow function that returns four instead. How does that work. Maybe its correct because the indexTest already defines x for us. 
+
+
+
+
+
+// const square = function(x = 2){
+//    return 2 * 2
+//}
+
+// this is whats wanted, but not in arrow form
+
+
+const add = (a, b) => a + b
+
+
+ 
+
+
+
