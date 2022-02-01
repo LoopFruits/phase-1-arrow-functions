ad1d6ad858fed2f2c9adf75aae2d0e898fb53079
Kenneth Jordan <kensworldc@gmail.com>
2022-02-01 12:24:21 -0500

    made code efficient

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-const divide = function(){
-    return 2000 / 100
+function divide() {
+    return 2000 / 100;
 }
 
-const square = x => 4
-// the question is asking for a parameter that multiplies itself to equal for. but i just wrote an arrow function that returns four instead. How does that work. Maybe its correct because the indexTest already defines x for us. 
+const square = x => x * 2 // we get two because we're squaring it and the test requires 4 as our answer. " multiplies it times itself"
 
 
-
-
-
-// const square = function(x = 2){
-//    return 2 * 2
-//}
-
-// this is whats wanted, but not in arrow form
-
-
-const add = (a, b) => a + b
-
-
- 
+ const add = (a , b) => a + b
 
 
 
